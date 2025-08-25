d49fd05ccd5e0e613fbfb595059195b9bfc6f98c
Chaya G <168974001+ChayaChandan@users.noreply.github.com>
2025-08-25 10:20:24 +0530

    test-db.js

---
+++ test-db.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect(err => {
+  if (err) console.error("❌ Connection failed:", err);
+  else console.log("✅ Connected to MySQL database!");
+});
